16995f494f514d7d58d0b91adff8fe854f7a8ddd
HaPhuong-krys <haphuong141198@gmail.com>
2021-11-30 10:40:20 +0100

    ajouter projet web 1

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>AccroCine</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="index.css">
+        <script language="javascript" type="text/javascript" src="validemail.js"></script>
+    </head>
+    <body>
+        <header >
+            <h1>AccroCine</h1>
+        </header>
+
+        <nav>
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li class="deroulant">Catégorie de film
+                    <ul class="sous">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Comédie</a></li>
+                        <li><a href="#">Thriler</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Inscription</a></li>
+                <li><a href="#">Se connecter</a></li>
+            </ul>
+        </nav>
+
+        <br><br>
+        <main>
+            <p>
+                AccroCiné, le site de référence du cinéma et des séries tv ! Découvrez notre recherche de films, 
+                le programme tv de vos séries préférées, l'actualité ciné et séries, les émissions AccroCiné, les dernières bandes-annonces, 
+                et plus encore. Laissez vous émerveiller et allez à la rencontre de nouveaux films.
+                </p>
+        </main>
+
+        <div class="d1"></div>
+<div class="conteneur">
+  <div class="d2"></div>
+</div>
+        
